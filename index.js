5d43934d91c3ce9d5626cb4c39029483db5c1da5
Sachin Kesarwani <112760394+Sachin-Kesarwani@users.noreply.github.com>
2023-07-14 18:35:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ let express=require("express")
 let {Server}=require('socket.io')
 let app=express()
 let http=require("http")
-
-
+let cors=require("cors")
+app.use(cors())
 let server=http.createServer(app)
 app.get("/",(req,res)=>{
     res.send({msg:"getting response"})
@@ -16,15 +16,22 @@ app.get("/",(req,res)=>{
 server.listen(8080,()=>{
     console.log("server is running")
 })
-
-let io=new Server(server)
+// module.exports=server
+// let io=new Server(server)
 let count=0
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
+console.log("hii")
 io.on("connection",(socket)=>{
  count++
-    
+    console.log("connection")
  //socket.broadcast.emit("online",count)        //It sends count  to everyone except newone
- //io.emit("online",count)     //It sends count value to everyone
-   io.emit("online",count)   //It sends count value to newone not to everyone
+ io.emit("online",count)     //It sends count value to everyone
+  // socket.emit("online",count)   //It sends count value to newone not to everyone
   socket.emit("blah","hello from server 1")
 
 
@@ -32,13 +39,35 @@ io.on("connection",(socket)=>{
   
     io.emit("zyx",msg)
   })
+///////////////////////////////////////////////you tube////////////////////
+console.log(socket.id)
+socket.emit("me", socket.id);
+
+socket.on("disconnect", () => {
+  count--
+  socket.broadcast.emit("callEnded")
+  io.emit("online",count) 
+});
+
+socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+ 
+  io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+});
+
+socket.on("answerCall", (data) => {
+ 
+  io.to(data.to).emit("callAccepted", data.signal)
+});
 
 
-  socket.on("disconnect",()=>{
-    count--
-    //socket.broadcast.emit("online",count)//It sends to everyone except newone
- io.emit("online",count)     //It sends count value to everyone
-  })
+///////////////////////////////////////////////////////////////////////
+
+
+//   socket.on("disconnect",()=>{
+//     count--
+//     //socket.broadcast.emit("online",count)//It sends to everyone except newone
+//  io.emit("online",count)     //It sends count value to everyone
+//   })
 
   
 })
